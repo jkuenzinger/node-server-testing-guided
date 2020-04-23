6817c3afaf7b7f6496f108fb4ab71e6c95634b14
jkuenzinger <jkuenzinger228@gmail.com>
2020-04-23 10:01:03 -0700

    asdf

--- knexfile.js
+++ knexfile.js
@@ -27,14 +27,14 @@ module.exports = {
     }
   },
 
-production: {
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  migrations: {
-    directory: "./data/migrations",
-  },
-  seeds: {
-    directory: "./data/seeds",
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    }
   }
-}
 }
